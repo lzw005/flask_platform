7ae8fca7ddf9d168270b2ecea97018e208603539
zyhbili <40693956+zyhbili@users.noreply.github.com>
2018-09-04 18:18:12 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,15 +3,15 @@ from datetime import timedelta
 
 class Config(object):
     DEBUG=True
-    SQLALCHEMY_DATABASE_URI = 'mysql://Ubuntu:123456@106.14.140.30:3306/test'
+    SQLALCHEMY_DATABASE_URI = ''
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'asajsfa1o3mfp1p3kpwofkwpkfpewkeopskpda'
+    SECRET_KEY = ''
     PERMANENT_SESSION_LIFETIME = timedelta(hours=3)  # 配置过期时间
 
     MAIL_SERVER = 'smtp.163.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
     MAIL_USE_TLS = False
-    MAIL_USERNAME = 'zyh_shdx@163.com'
-    MAIL_PASSWORD = 'zyh19980310'
-    MAIL_DEFAULT_SENDER = 'zyh_shdx@163.com'
+    MAIL_USERNAME = ''
+    MAIL_PASSWORD = ''
+    MAIL_DEFAULT_SENDER = ''
